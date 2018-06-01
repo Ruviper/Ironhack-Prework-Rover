8b921d6514146c431e213da48d29c20a393301df
Ruben <rubenvilchesperez1985@hotmail.com>
2018-06-01 16:53:10 +0200

    add files

---
+++ javascripts/app.js
@@ -0,0 +1,16 @@
+var roberObject {
+  direction: "N"
+}
+
+
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
